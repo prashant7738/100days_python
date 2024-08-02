f1067a1f0505b6eeb5577f68b2bc36c76ec2ba93
Prashant Kafle <prashantkafle7738@gmail.com>
2024-08-02 09:00:49 +0545

    Update send_msg.py

--- Day_24/send_msg.py
+++ Day_24/send_msg.py
@@ -1,13 +1,1 @@
-# from twilio.rest import Client
-
-# account_sid = 'sid'
-# auth_token = 'token'
-# client = Client(account_sid, auth_token)
-
-# message = client.messages.create(
-#   from_='number',
-#   to='number',
-#   body='Hello its me Prashant'
-# )
-
-# print(message.sid)
+# I use twilio to send message to my phone
