a7432a6a3ffd402aac1d95a2bdc87f13c96bb53f
Prashant Kafle <prashantkafle7738@gmail.com>
2024-12-06 18:07:19 +0545

    12/6
    Revised Django upto models. I am creating a portfolio website

--- Day_44/myproject/myproject/urls.py
+++ Day_44/myproject/myproject/urls.py
@@ -1,10 +1,11 @@
 from . import views
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page),
     path('about/',views.about ),
+    path('projects/' , include('projects.urls'))
 ]
---
+++ Day_44/myproject/projects/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def projects_list(request):
+    return render(request,'projects/projects_list.html')
